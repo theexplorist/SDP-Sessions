2a93d2a37e9ef80378d9cc20c0efe6bc0d7f6097
Kartik Sapra <36664705+theexplorist@users.noreply.github.com>
2021-12-07 10:01:32 +0530

    Update DTt.java

--- DTt.java
+++ DTt.java
@@ -142,3 +142,246 @@ class Solution {
         return Math.max(with, without);
     }
 }
+************************************************************************
+
+//BS1
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int[][] dp = new int[prices.length + 1][2];
+        
+        for(int i = 0; i <= prices.length; i++) {
+            dp[i][0] = -1;
+            dp[i][1] = -1;
+        }
+        return sol(prices, 0, 1, 1, dp);
+    }
+    
+    public int sol(int[] prices, int curr, int buy, int k, int[][] dp) {
+        
+        if(curr == prices.length || k == 0) {
+            return 0;
+        }
+        
+        if(dp[curr][buy] != -1) {
+            return dp[curr][buy];
+        }
+        if(buy == 1) {
+            int inc = sol(prices, curr + 1, 0, k, dp) - prices[curr];
+            int exc = sol(prices, curr + 1, buy, k, dp);
+            
+            return dp[curr][buy] = Math.max(inc, exc);
+        } else {
+            int inc = sol(prices, curr + 1, buy, k - 1, dp) + prices[curr];
+            int exc = sol(prices, curr + 1, buy, k, dp);
+            
+            return dp[curr][buy] = Math.max(inc, exc);
+        }
+    }
+}
+
+********************************************************************
+
+//BS2
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length + 1][2];
+        
+        for(int i = 0; i <= prices.length; i++) {
+            dp[i][0] = -1;
+            dp[i][1] = -1;
+        }
+        
+        return sol(prices, 0, 1, dp);
+    }
+    
+    public int sol(int[] prices, int curr, int buy, int[][] dp) {
+        
+        if(curr == prices.length) {
+            return 0;
+        }
+        
+        if(dp[curr][buy] != -1) {
+            return dp[curr][buy];
+        }
+        if(buy == 1) {
+            int inc = sol(prices, curr + 1, 0, dp) - prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp);
+            
+            return dp[curr][buy] = Math.max(inc, exc);
+        } else {
+            int inc = sol(prices, curr + 1, 1, dp) + prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp);
+            
+            return dp[curr][buy] = Math.max(inc, exc);
+        }
+    }
+}
+
+//BS3
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][][] dp = new int[prices.length + 1][2][3];
+        
+        for(int i = 0; i <= prices.length; i++) {
+            dp[i][0][0] = -1;
+            dp[i][0][1] = -1;
+            dp[i][0][2] = -1;
+            dp[i][1][0] = -1;
+            dp[i][1][1] = -1;
+            dp[i][1][2] = -1;
+        }
+        return sol(prices, 0, 1, dp, 0);
+    }
+    
+    public int sol(int[] prices, int curr, int buy, int[][][] dp, int t) {
+        
+        if(curr == prices.length || t >= 2) {
+            return 0;
+        }
+        
+        if(dp[curr][buy][t] != -1) {
+            return dp[curr][buy][t];
+        }
+        if(buy == 1) {
+            int inc = sol(prices, curr + 1, 0, dp, t) - prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp, t);
+            
+            return dp[curr][buy][t] = Math.max(inc, exc);
+        } else {
+            int inc = sol(prices, curr + 1, 1, dp, t + 1) + prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp, t);
+            
+            return dp[curr][buy][t] = Math.max(inc, exc);
+        }
+    }
+}
+
+//BS4
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int[][][] memo = new int[prices.length][2][k + 1];
+
+        for (int r = 0; r < memo.length; r++) {
+            for (int c = 0; c < memo[r].length; c++) {
+                Arrays.fill(memo[r][c], -1);
+            }
+        }
+
+        return sol(prices, 0, 1, memo, 0, k);
+    }
+
+   public int sol(int[] prices, int curr, int buy, int[][][] dp, int t, int k) {
+        
+        if(curr == prices.length || t >= k) {
+            return 0;
+        }
+        
+        if(dp[curr][buy][t] != -1) {
+            return dp[curr][buy][t];
+        }
+        if(buy == 1) {
+            int inc = sol(prices, curr + 1, 0, dp, t, k) - prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp, t, k);
+            
+            return dp[curr][buy][t] = Math.max(inc, exc);
+        } else {
+            int inc = sol(prices, curr + 1, 1, dp, t + 1, k) + prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp, t, k);
+            
+            return dp[curr][buy][t] = Math.max(inc, exc);
+        }
+   }
+}
+
+//BS5
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length + 1][2];
+        
+        for(int i = 0; i <= prices.length; i++) {
+            dp[i][0] = -1;
+            dp[i][1] = -1;
+        }
+        
+        return sol(prices, 0, 1, dp);
+    }
+    
+    public int sol(int[] prices, int curr, int buy, int[][] dp) {
+        
+        if(curr >= prices.length) {
+            return 0;
+        }
+        
+        if(dp[curr][buy] != -1) {
+            return dp[curr][buy];
+        }
+        if(buy == 1) {
+            int inc = sol(prices, curr + 1, 0, dp) - prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp);
+            
+            return dp[curr][buy] = Math.max(inc, exc);
+        } else {
+            int inc = sol(prices, curr + 2, 1, dp) + prices[curr];
+            int exc = sol(prices, curr + 1, buy, dp);
+            
+            return dp[curr][buy] = Math.max(inc, exc);
+        }
+    }
+}
+
+//Knight Dialer
+
+class Solution {
+    
+    int[] dx = {-1, 1, -2, 2, -2, 2, -1, 1};
+    int[] dy = {-2, -2, -1, -1, 1, 1, 2, 2};
+    public int knightDialer(int n) {
+        
+        int[][][] dp = new int[n + 1][4][3];
+        
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j < 4; j++) {
+                for(int k = 0; k < 3; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        int ans = 0;
+        for(int r = 0; r < 4; r++) {
+            for(int c = 0; c < 3; c++) {
+                if((r == 3 &&  c == 0) || (r == 3 && c == 2)) {
+                    
+                } else {
+                    ans =  (ans + knightDialerR(n - 1, r, c, dp)) % 1000000007;
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    public int knightDialerR(int n, int r, int c, int[][][] dp) {
+        
+        if(r < 0 || c < 0 || r > 3 || c > 2 || (r == 3 &&  c == 0) || (r == 3 && c == 2)) {
+            return 0;
+        }
+        if(n == 0) {
+            return 1;
+        }
+        
+        if(dp[n][r][c] != -1) {
+            return dp[n][r][c];
+        }
+        int ct = 0;
+        for(int i = 0; i < 8; i++) {
+            ct = (ct + knightDialerR(n - 1, r + dx[i], c + dy[i], dp)) % 1000000007;
+        }
+        
+        return dp[n][r][c] = (ct % 1000000007);
+    }
+}
